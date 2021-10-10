28784adbc0f616a7db067bdf179c010a645d8bd9
Chanakya <sunkarapallychanakya@gmail.com>
2021-10-10 07:26:08 +0530

    fix - upload download dirs ci.yml, Dockerfile ver.11

--- scripts/dockerScript.sh
+++ scripts/dockerScript.sh
@@ -5,8 +5,8 @@ printenv | \
  while read -r line; do
    key=$(echo "$line" | cut -d "=" -f1)
    value=$(echo "$line" | cut -d "=" -f2)
-   if [[ "${env_keys[*]}" =~ $key ]]; then
-    printf "%s=%s\n" "$key" "$value" > env_vars.list
+   if [[ "${env_keys[*]}" == "$key" ]]; then
+    printf "%s=%s\n" "$key" "$value" >> env_vars.list
    fi
   done
 cat env_vars.list
